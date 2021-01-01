f00b148679122141ef1dff523feabfae97db4b43
AbdLaswi <abed.laswii@gmail.com>
2021-01-01 21:49:52 +0200

    try to solve P4 and move to P1

--- part_4/part_4.js
+++ part_4/part_4.js
@@ -11,12 +11,21 @@ return 'NO' if the parentheses in the input string are not balanced,.
 
 console.log('PART 4');
 
-const isABalancedString = () => {
+const isABalancedString = (str) => {
   // WRITE YOUR CODE BELOW THIS LINE
+  str =str.split("")
+  for(let i =0; i < str.length; i++){
+    
+  }
+  
 };
 
 /*
-
+every bracket that we open should be closed to return YES otherwise it will show NO 
+so let's cut it to small pieces
+1- the code should check every single ele in the arr (for statement) 
+2- after that, the code must catch the open bracket and check if there any close bracket that's the same 
+3- i
 Examples:
 
 isABalancedString( '[]' )
