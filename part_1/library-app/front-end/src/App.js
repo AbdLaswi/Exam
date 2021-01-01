058105aa1f74f6dba589a1a2f6e7817b8e4bdd6b
AbdLaswi <abed.laswii@gmail.com>
2021-01-01 22:27:53 +0200

    try to solve P1 and move to P4

--- part_1/library-app/front-end/src/App.js
+++ part_1/library-app/front-end/src/App.js
@@ -1,22 +1,16 @@
-import React,{useState,useEffect} from 'react'
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function App() {
- const [book, setBook] = useState({
-   title: "",
-   author:"",
-   pages:"",
-   publisher: "",
-   published_at:"",
- });
+  const [book, setBook] = useState({
+    title: "",
+    author: "",
+    pages: "",
+    publisher: "",
+    published_at: "",
+  });
 
- axios
- .get("http://localhost:3000/books");
+  axios.get("http://localhost:3000/books");
 
- })
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
