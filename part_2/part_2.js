f1e7b8a114679a2683c41f4cd027f7a218924c80
AbdLaswi <abed.laswii@gmail.com>
2021-01-01 21:29:24 +0200

    solve most questions and move to think and try to solve  P4

--- part_2/part_2.js
+++ part_2/part_2.js
@@ -5,36 +5,36 @@ PART 2 [SQL Queries]:
 Type the correct SQL query below every question:
 
 Q1) What is the query for displaying all the databases?
-
+SHOW DATABASES
 
 Q2) What is the query for using a specific database?
-
+SELECT DATABASE
 
 Q3) What is the query for creating a new database called `meraki`?
-
+CREATE DATABASE meraki 
 
 Q4) What is the query for deleting a database called `meraki`?
-
+DROP meraki
 
 Q5) What is the query for displaying all the tables for a database called `meraki`?
-
+SHOW Tables
 
 Q6) What is the query for displaying the columns of a table called `users`?
-
+SELECT * FROM users
 
 Q7) What is the query to insert information into a table called `users` that has four columns (name, email, password, age)?
-
+INSERT INTO users (name, email, password, age) VALUES (?,?,?,?)
 
 Q8) What is the query to update the `name` of a user that has `info@meraki-academy.org` as an email?
 
-
+UPDATE  user SET name = ? WHERE email = info@meraki-academy.org`
 Q9) What is the query to delete a user that has `info@meraki-academy.org` as an email?
-
+DELETE FROM users WHERE email = `info@meraki-academy.org`
 
 Q10) What is the query to perform a left join on two tables (users, posts) depending on the user_id? (user_id is a FK in `posts` table)
-
+SELECT user_id, post_id from users WHERE users_user_id = post_post_id
 
 Q11) What is the query to get all the users that their age ranges from 20-30 from the `users` table?
-
+SELECT * FROM users WHERE age >20 AND age< 30
 
 */
--- part_3/part_3.js
+++ part_3/part_3.js
@@ -5,32 +5,34 @@ PART 3 [General Information]:
 Type the correct answer below each question:
 
 Q1) Why do we use joins in SQL?
-
+to chose specific data from Two tables and select them as one table 
 
 Q2) Name three types of relationships in databases.
-
+one to one , one to many , many to many
 
 Q3) What are the main three sections of a JWT?
-
+Payloads, Option, SECRETE
 
 Q4) Name three types of express middle-wares.
 
 
 Q5) What is the `effect` hook used for in react?
-
+to render the page when the user enter the page.
 
 Q6) What is the `state` hook used for in react?
-
+to save the values that users inter in typing box
 
 Q7) List three thing you can use to create a responsive web application.
 
 
 Q8) Name two of mongoose middle-wares and explain what they are used for
-
+updateOne to update the value in the schema
+deleteOne to delete the value from the schema
 
 Q9) What is the difference between authentication and authorization?
 
 
 Q10) Why do we save some information as an environment variable (in .env file)?
-
+to make our application more secure, if anyone can be allowed to have access in the database
+then users data will be public.
 */
